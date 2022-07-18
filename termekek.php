9885c276b6ac13c41ac019e72b705b57fdc43d19
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-18 11:40:39 +0200

    Organized the image files and turned the html files to php files

---
+++ termekek.php
@@ -0,0 +1,396 @@
+<?php include('inc/header.php'); ?>
+
+<div class="container-fluid" style="margin-top:50px">
+    <h3 class="h3">Porsche 911</h3>
+    <div class="row">
+        <div class="col-md-3 col-sm-6" id="product_card">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+
+                        <img class="pic-1" src="porsche_911.JPG">
+
+                        <ul class="pic-2">
+                            <li>8,9 l/100 km</li>
+                            <li>3,7 mp </br>Gyorsulás 0 - 100 km/h</li>
+                            <li>450 Le</li>
+                            <li>308 km/h Végsebesség</li>
+                            <li>1,852 mm Szélesség</li>
+                            <li>4,519 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+
+                    <h3 class="title">Porsche 911 Carrera S (2019)</h3>
+                    <div class="price">138 376 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_911_targa_4.jpg">
+                        <ul class="pic-2">
+                            <li>8,3 l/100 km</li>
+                            <li>4,7 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>370 Le</li>
+                            <li>289 km/h Végsebesség</li>
+                            <li>1,852 mm Szélesség</li>
+                            <li>4,499 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+
+                    <h3 class="title">Porsche 911 Targa 4</h3>
+                    <div class="price">124 943 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_911_t_c.jpg">
+                        <ul class="pic-2">
+                            <li>9,3 l/100 km</li>
+                            <li>3,1 mp Gyorsulás <br> 0 - 100 km/h</li>
+                            <li>540 Le</li>
+                            <li>320 km/h Végsebesség</li>
+                            <li>1,880 mm Szélesség</li>
+                            <li>4,507 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title">Porsche 911 Turbo Cabriolet</h3>
+                    <div class="price">186 411 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_911_gt2_rs.JPG">
+                        <ul class="pic-2">
+                            <li>11,8 l/100 km</li>
+                            <li>2,8 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>700 Le</li>
+                            <li>340 km/h Végsebesség</li>
+                            <li>1,880 mm Szélesség</li>
+                            <li>4,549 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title">Porsche 911 GT2 RS</h3>
+                    <div class="price">275 000 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container" style="margin-top:50px">
+    <h3 class="h3">Porsche 718</h3>
+    <div class="row">
+        <div class="col-md-3 col-sm-6" id="product_card">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+
+                        <img class="pic-1" src="porsche_718.JPG">
+
+                        <ul class="pic-2">
+                            <li>8,5 l/100 km</li>
+                            <li>4,6 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>350 Le</li>
+                            <li>285 km/h Végsebesség</li>
+                            <li>1,295 mm Szélesség</li>
+                            <li>4,379 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+
+                    <h3 class="title">Porsche 718 Cayman S</h3>
+                    <div class="price">68 428 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_718_boxster.jpg">
+                        <ul class="pic-2">
+                            <li>8,1 l/100 km</li>
+                            <li>5,1 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>300 Le</li>
+                            <li>275 km/h Végsebesség</li>
+                            <li>1,801 mm Szélesség</li>
+                            <li>4,379 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+
+                    <h3 class="title">Porsche 718 Boxster</h3>
+                    <div class="price">57 252 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_718_t_cayman.jpg">
+                        <ul class="pic-2">
+                            <li>8,1 l/100 km</li>
+                            <li>4,7 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>300 Le</li>
+                            <li>275 km/h Végsebesség</li>
+                            <li>1,801 mm Szélesség</li>
+                            <li>4,379 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title">Porsche 718 T Cayman</h3>
+                    <div class="price">63 000 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_718_boxster_gts.jpg">
+                        <ul class="pic-2">
+                            <li>9,2 l/100 km</li>
+                            <li>4,6 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>365 Le</li>
+                            <li>290 km/h Végsebesség</li>
+                            <li>1,801 mm Szélesség</li>
+                            <li>4,379 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title">Porsche 718 Boxster GTS</h3>
+                    <div class="price">73 427 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container" style="margin-top:50px">
+    <h3 class="h3">Porsche Panamera</h3>
+    <div class="row">
+        <div class="col-md-3 col-sm-6" id="product_card">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+
+                        <img class="pic-1" src="porsche_panamera_4.jpg">
+
+                        <ul class="pic-2">
+                            <li>9,3 l/100 km</li>
+                            <li>5,2 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>330 Le</li>
+                            <li>260 km/h Végsebesség</li>
+                            <li>1,938 mm Szélesség</li>
+                            <li>5,049 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+
+                    <h3 class="title">Porsche Panamera 4</h3>
+                    <div class="price">79 828 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_panamera_gts_st.jpg">
+                        <ul class="pic-2">
+                            <li>8,2 l/100 km</li>
+                            <li>4,6 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>460 Le</li>
+                            <li>289 km/h Végsebesség</li>
+                            <li>1,937 mm Szélesség</li>
+                            <li>5,053 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+
+                    <h3 class="title">Panamera GTS Sport Turismo</h3>
+                    <div class="price">140 019 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_panamera_turbo_ex.jpg">
+                        <ul class="pic-2">
+                            <li>10,6 l/100 km</li>
+                            <li>3,9 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>460 Le</li>
+                            <li>306 km/h Végsebesség</li>
+                            <li>1,937 mm Szélesség</li>
+                            <li>5,199 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title">Porsche Panamera Turbo Executive</h3>
+                    <div class="price">177 419 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_panamera_ehybrid.jpg">
+                        <ul class="pic-2">
+                            <li>2,5 l/100 km</li>
+                            <li>4,6 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>330+136 Le</li>
+                            <li>278 km/h Végsebesség</li>
+                            <li>1,937 mm Szélesség</li>
+                            <li>5,049 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title">Porsche Panamera 4 E-Hybrid</h3>
+                    <div class="price">114 783 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container" style="margin-top:50px">
+    <h3 class="h3">Porsche terepjárók</h3>
+    <div class="row">
+        <div class="col-md-3 col-sm-6" id="product_card">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+
+                        <img class="pic-1" src="porsche_macan2019.jpg">
+
+                        <ul class="pic-2">
+                            <li>8,1 l/100 km</li>
+                            <li>6,7 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>245 Le</li>
+                            <li>225 km/h Végsebesség</li>
+                            <li>1,923 mm Szélesség</li>
+                            <li>4,696 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+
+                    <h3 class="title">Porsche Macan (2019)</h3>
+                    <div class="price">78 641 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_macan_s2019.jpg">
+                        <ul class="pic-2">
+                            <li>8,9 l/100 km</li>
+                            <li>5,3 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>354 Le</li>
+                            <li>254 km/h Végsebesség</li>
+                            <li>1,923 mm Szélesség</li>
+                            <li>4,696 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+
+                    <h3 class="title">Porsche Macan S (2019)</h3>
+                    <div class="price">86 481 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_cayanne2019.jpg">
+                        <ul class="pic-2">
+                            <li>9,2 l/100 km</li>
+                            <li>6,2 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>340 Le</li>
+                            <li>245 km/h Végsebesség</li>
+                            <li>1,983 mm Szélesség</li>
+                            <li>4,918 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title">Porsche Cayanne (2019)</h3>
+                    <div class="price">95 150 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid9">
+                <div class="product-image9">
+                    <a href="#">
+                        <img class="pic-1" src="porsche_cayanne_ehybrid2019.jpg">
+                        <ul class="pic-2">
+                            <li>3,4 l/100 km</li>
+                            <li>5,0 mp Gyorsulás </br> 0 - 100 km/h</li>
+                            <li>340+136 Le</li>
+                            <li>253 km/h Végsebesség</li>
+                            <li>1,983 mm Szélesség</li>
+                            <li>4,918 mm Hossz</li>
+                        </ul>
+                    </a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title">Porsche Cayanne E-Hybrid (2019)</h3>
+                    <div class="price">70 886 EUR</div>
+                    <a class="add-to-cart" href="kapcsolat.html">Vásárlás</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('inc/footer.php'); ?>
+
+</body>
+
+</html>
